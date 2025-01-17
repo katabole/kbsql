5898be7a6382ae9ae94827004ed00607445c06a9
dkinder <dkinder.is.me@gmail.com>
2025-01-17 16:07:31 -0500

    Ensure atlas_dev exists before trying to apply
    Needed in places like CI where it's a little more annoying to make sure
    setup tasks are already run.

--- atlas.go
+++ atlas.go
@@ -3,10 +3,17 @@ package kbsql
 import (
 	"fmt"
 	"os/exec"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // AtlasSetupDB runs atlas to ensure the database (usually dev/test) is up to date.
 func AtlasSetupDB(dbURL string, atlasDevDBURL string) error {
+	if err := ensureAtlasDevExists(atlasDevDBURL); err != nil {
+		return err
+	}
+
 	cmd := exec.Command("atlas", "schema", "apply",
 		"--to", "file://schema.sql",
 		"--url", dbURL,
@@ -16,6 +23,29 @@ func AtlasSetupDB(dbURL string, atlasDevDBURL string) error {
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("Error running atlas schema apply: %v\n\nOutput: %s\n", err, output)
+	}
+	return nil
+}
+
+// ensureAtlasDevExists creates atlas_dev if needed, required to run atlas commands.
+// `task setup` creates this but that doesn't run for CI
+func ensureAtlasDevExists(atlasDevDBURL string) error {
+	db, err := sqlx.Connect("pgx", strings.ReplaceAll(atlasDevDBURL, "atlas_dev", ""))
+	if err != nil {
+		return fmt.Errorf("failed to connect to atlas dev db: %w", err)
+	}
+	defer db.Close()
+
+	var results []int
+	err = db.Select(&results, `SELECT 1 FROM pg_database WHERE datname = 'atlas_dev'`)
+	if err != nil {
+		return fmt.Errorf("error selecting tables from information schema: %w", err)
+	}
+	if len(results) == 0 {
+		_, err := db.Exec("CREATE DATABASE atlas_dev")
+		if err != nil {
+			return fmt.Errorf("error creating atlas_dev database: %w", err)
+		}
 	}
 	return nil
 }
